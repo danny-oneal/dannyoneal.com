be697f9cfeff62f058e110a583785f7264c1fb80
danny-oneal <danny.e.oneal@gmail.com>
2023-05-30 21:54:32 -0700

    favicon; og;

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
 				<h2 className="text-sm text-zinc-500 ">
-					Hi my name is Danny O'Neal. I am a Software Development Engineer.{" "}
+					Hi my name is Danny O'Neal. I am a Software Engineer.{" "}
 					{/* <Link
 						target="_blank"
 						href="https://upstash.com"
